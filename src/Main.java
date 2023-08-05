ac0cff9ddd8fb9d7c15ea46b3cf237918d3420ce
AnastasiyaPod <an_pod12@mail.ru>
2023-08-06 00:26:25 +1000

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int acct = 300;
+        int refill = 1100;
+        int bonus;
+        if (refill >= 1000) {
+            bonus = refill / 100;
+        } else {
+            bonus = 0;
+        }
+        System.out.println("Количество средств на счете:" + (acct + refill + bonus));
+        System.out.println("Количество начисленных бонусов:" + bonus);
+    }
+}
